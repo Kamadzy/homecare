9f0fe731cdf341d5ac0ca0b0f0073a36393659af
Kamadzy <kam4513@gmail.com>
2017-04-17 18:20:31 +0300

    added formspree and some styles

--- js/app.js
+++ js/app.js
@@ -6,4 +6,26 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
         dots: false,
 });
+
+    $('#form-group').submit(function(e){
+        var subject = document.getElementById('subject');
+        var name = document.getElementById('name');
+        var message = document.getElementById('message');
+
+        if(!subject.value || !name.value || !message.value){
+            alert("Check your entries");
+        }else{
+            $.ajax({
+                method: 'POST',
+                url: '//formspree.io/kam4513@gmail.com',
+                data: $('#form-group').serialize(),
+                datatype: 'json'
+            });
+            e.preventDefault();
+            $(this).get(0).reset();
+            alert("Message sent");
+        }
+
+    })
+
 });
